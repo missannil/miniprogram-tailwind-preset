46e805ccad0fbea6cf1a131108f3e9f9e727363e
missannil <missannil@163.com>
2023-05-29 16:54:16 +0800

    chore: 准备发布

--- tailwindcss-preset/plugins/baseStyle.ts
+++ tailwindcss-preset/plugins/baseStyle.ts
@@ -1,6 +1,6 @@
 import type { PluginCreator } from 'tailwindcss/types/config';
 
-export const baseStyle: PluginCreator = ({ addBase, theme }) => {
+export const baseStyle: PluginCreator = ({ addBase }) => {
 	addBase({
 		'view,[class]': { 'box-sizing': 'border-box'},
 	});
