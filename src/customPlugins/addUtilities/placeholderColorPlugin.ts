929d140e47f2e51b32c0653f73d919ec84961577
missanni <missannil@163.com>
2024-01-12 17:29:42 +0800

    ci: add test.yml release-please.yml remove on PR

--- src/customPlugins/addUtilities/placeholderColorPlugin.ts
+++ src/customPlugins/addUtilities/placeholderColorPlugin.ts
@@ -1,34 +1,34 @@
-import type { PluginCreator } from "tailwindcss/types/config";
-import { flattenColorPalette } from "../../utils/flattenColorPalette";
-import { isEmptyObject } from "../../utils/isEmptyObject";
-export const placeholderColorPlugin: PluginCreator = ({
-  theme,
-  matchUtilities,
-}) => {
-  const defaultTheme: object = theme("colors");
-  const variableColors: {
-    text: Record<string, Record<string, string>>;
-    common: Record<string, Record<string, string>>;
-  } = theme("variableColors");
-  const colorsTheme = isEmptyObject(defaultTheme)
-    ? Object.assign(variableColors.common, variableColors.text)
-    : defaultTheme;
-  matchUtilities(
-    {
-      placeholder: (value): any => {
-        return {
-          "&::placeholder": {
-            color: value,
-          },
-        };
-      },
-    },
-    {
-      values: flattenColorPalette(colorsTheme),
-      type: ["color", "any"],
-    },
-  );
-  // addUtilities({
-  // 	...createValue(flattenColorPalette(colorsTheme), '::caret', 'caretColor'),
-  // });
-};
+// import type { PluginCreator } from "tailwindcss/types/config";
+// import { flattenColorPalette } from "../../utils/flattenColorPalette";
+// import { isEmptyObject } from "../../utils/isEmptyObject";
+// export const placeholderColorPlugin: PluginCreator = ({
+//   theme,
+//   matchUtilities,
+// }) => {
+//   const defaultTheme: object = theme("colors");
+//   const variableColors: {
+//     text: Record<string, Record<string, string>>;
+//     common: Record<string, Record<string, string>>;
+//   } = theme("variableColors");
+//   const colorsTheme = isEmptyObject(defaultTheme)
+//     ? Object.assign(variableColors.common, variableColors.text)
+//     : defaultTheme;
+//   matchUtilities(
+//     {
+//       placeholder: (value): any => {
+//         return {
+//           "&::placeholder": {
+//             color: value,
+//           },
+//         };
+//       },
+//     },
+//     {
+//       values: flattenColorPalette(colorsTheme),
+//       type: ["color", "any"],
+//     },
+//   );
+//   // addUtilities({
+//   // 	...createValue(flattenColorPalette(colorsTheme), '::caret', 'caretColor'),
+//   // });
+// };
