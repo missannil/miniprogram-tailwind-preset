db8f3775c20e4e68519421d68410cfc72283c8d9
missanni <missannil@163.com>
2024-01-12 17:29:42 +0800

    ci: add test.yml release-please.yml remove on PR

--- src/customPlugins/addUtilities/backgroundColorPlugin.ts
+++ src/customPlugins/addUtilities/backgroundColorPlugin.ts
@@ -1,24 +1,24 @@
-import type { PluginCreator } from "tailwindcss/types/config";
-import { createValue } from "../../utils/createValue";
-import { flattenColorPalette } from "../../utils/flattenColorPalette";
-import { isEmptyObject } from "../../utils/isEmptyObject";
-export const backgroundColorPlugin: PluginCreator = ({
-  addUtilities,
-  theme,
-}) => {
-  const defaultTheme: object = theme("colors");
-  const variableColors: {
-    bg: Record<string, Record<string, string>>;
-    common: Record<string, Record<string, string>>;
-  } = theme("variableColors");
-  const colorsTheme = isEmptyObject(defaultTheme)
-    ? Object.assign(variableColors.common, variableColors.bg)
-    : defaultTheme;
-  addUtilities({
-    ...createValue(
-      flattenColorPalette(colorsTheme),
-      "bg",
-      "backgroundColor",
-    ),
-  });
-};
+// import type { PluginCreator } from "tailwindcss/types/config";
+// import { createValue } from "../../utils/createValue";
+// import { flattenColorPalette } from "../../utils/flattenColorPalette";
+// import { isEmptyObject } from "../../utils/isEmptyObject";
+// export const backgroundColorPlugin: PluginCreator = ({
+//   addUtilities,
+//   theme,
+// }) => {
+//   const defaultTheme: object = theme("colors");
+//   const variableColors: {
+//     bg: Record<string, Record<string, string>>;
+//     common: Record<string, Record<string, string>>;
+//   } = theme("variableColors");
+//   const colorsTheme = isEmptyObject(defaultTheme)
+//     ? Object.assign(variableColors.common, variableColors.bg)
+//     : defaultTheme;
+//   addUtilities({
+//     ...createValue(
+//       flattenColorPalette(colorsTheme),
+//       "bg",
+//       "backgroundColor",
+//     ),
+//   });
+// };
--- src/customPlugins/addUtilities/caretColorPlugin.ts
+++ src/customPlugins/addUtilities/caretColorPlugin.ts
@@ -1,17 +1,17 @@
-import type { PluginCreator } from "tailwindcss/types/config";
-import { createValue } from "../../utils/createValue";
-import { flattenColorPalette } from "../../utils/flattenColorPalette";
-import { isEmptyObject } from "../../utils/isEmptyObject";
-export const caretColorPlugin: PluginCreator = ({ addUtilities, theme }) => {
-  const defaultTheme: object = theme("colors");
-  const variableColors: {
-    text: Record<string, Record<string, string>>;
-    common: Record<string, Record<string, string>>;
-  } = theme("variableColors");
-  const colorsTheme = isEmptyObject(defaultTheme)
-    ? Object.assign(variableColors.common, variableColors.text)
-    : defaultTheme;
-  addUtilities({
-    ...createValue(flattenColorPalette(colorsTheme), "caret", "caretColor"),
-  });
-};
+// import type { PluginCreator } from "tailwindcss/types/config";
+// import { createValue } from "../../utils/createValue";
+// import { flattenColorPalette } from "../../utils/flattenColorPalette";
+// import { isEmptyObject } from "../../utils/isEmptyObject";
+// export const caretColorPlugin: PluginCreator = ({ addUtilities, theme }) => {
+//   const defaultTheme: object = theme("colors");
+//   const variableColors: {
+//     text: Record<string, Record<string, string>>;
+//     common: Record<string, Record<string, string>>;
+//   } = theme("variableColors");
+//   const colorsTheme = isEmptyObject(defaultTheme)
+//     ? Object.assign(variableColors.common, variableColors.text)
+//     : defaultTheme;
+//   addUtilities({
+//     ...createValue(flattenColorPalette(colorsTheme), "caret", "caretColor"),
+//   });
+// };
